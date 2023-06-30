a96dca9c1f2a9b2fbedff476ee4fd53bc04c4199
agastiayudya <adianfayu@gmail.com>
2023-06-30 13:05:09 +0700

    feat: Added Update Item functionality

--- lib/transaction.py
+++ lib/transaction.py
@@ -19,3 +19,68 @@ class Transaction:
             if inst.args[0] == 'duplicate_item':
                 print('Sudah ada item dengan nama yang sama.')
 
+    def update_item_name(self, item_name=None, updated_name=None):
+        try:
+            found_flag = False
+            
+            if(item_name==None or updated_name==None):
+                raise Exception('missing_field')
+            
+            for item in self.item_list:
+                if item['name'] == item_name:
+                    item['name'] = updated_name
+                    found_flag = True
+                    break
+                
+            if(not found_flag):
+                raise Exception('not_found')
+            
+        except Exception as inst:
+            if inst.args[0] == 'missing_field':
+                print('Pastikan memasukkan nama item dan nama yang akan diperbaharui.')
+            if inst.args[0] == 'not_found':
+                print('Item yang akan diperbaharui tidak ditemukan')
+    
+    def update_item_quantity(self, item_name=None, updated_quantity=None):
+        try:
+            found_flag = False
+            
+            if(item_name==None or updated_quantity==None):
+                raise Exception('missing_field')
+            
+            for item in self.item_list:
+                if item['name'] == item_name:
+                    item['quantity'] = updated_quantity
+                    found_flag = True
+                    break
+                
+            if(not found_flag):
+                raise Exception('not_found')
+            
+        except Exception as inst:
+            if inst.args[0] == 'missing_field':
+                print('Pastikan memasukkan nama item dan kuantitas yang akan diperbaharui.')
+            if inst.args[0] == 'not_found':
+                print('Item yang akan diperbaharui tidak ditemukan')
+
+    def update_item_price(self, item_name=None, price=None):
+        try:
+            found_flag = False
+            
+            if(item_name==None or price==None):
+                raise Exception('missing_field')
+            
+            for item in self.item_list:
+                if item['name'] == item_name:
+                    item['price'] = price
+                    found_flag = True
+                    break
+                
+            if(not found_flag):
+                raise Exception('not_found')
+            
+        except Exception as inst:
+            if inst.args[0] == 'missing_field':
+                print('Pastikan memasukkan nama item dan harga yang akan diperbaharui.')
+            if inst.args[0] == 'not_found':
+                print('Item yang akan diperbaharui tidak ditemukan')
--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from lib.transaction import Transaction
 transaction = Transaction()
 
 transaction.add_item("Permen", 2, 2000)
+print("Item List Sebelum Update")
+print(transaction.item_list)
+
+transaction.update_item_name("Permen", "Permen Mantab")
+transaction.update_item_quantity("Permen Mantab", 10)
+transaction.update_item_price("Permen Mantab", 2500)
+print("Item List Sesudah Update")
 print(transaction.item_list)
